363f1a643f52d47777b90d040e79fb89cbe21904
Kazushi Konosu <kazushi@konosu.me>
2023-05-24 01:27:27 +0900

    feat: add suport for custom class names

--- src/index.ts
+++ src/index.ts
@@ -9,10 +9,17 @@ const test = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'].map((tagName) => ({
 }));
 
 const rehypeHeadingAnchor: Plugin<
-  never[],
+  { className: string[] }[] | never[],
   Node<Element>,
   Node<Element>
-> = () => {
+> = (...parameters) => {
+  const className =
+    parameters.length > 0 &&
+    Array.isArray(parameters[0].className) &&
+    parameters[0].className.every((item) => typeof item === 'string')
+      ? parameters[0].className.filter((item) => !!item)
+      : [];
+
   return (ast) => {
     visit(ast, test, (node, index, parent) => {
       if (index === null || parent === null) {
@@ -31,7 +38,7 @@ const rehypeHeadingAnchor: Plugin<
         tagName: 'a',
         properties: {
           href: `#${id}`,
-          className: ['heading-anchor'],
+          ...(className.length > 0 ? { className } : {}),
         },
         children: [heading],
       };
--- test/index.test.ts
+++ test/index.test.ts
@@ -13,7 +13,7 @@ it('should add an anchor tag to every heading', async () => {
     ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'].map(async (tag) => {
       const result = await processor.process(`<${tag}>Hello world</${tag}>`);
       expect(result.toString()).toBe(
-        `<a href="#hello-world" class="heading-anchor"><${tag} id="hello-world">Hello world</${tag}></a>`,
+        `<a href="#hello-world"><${tag} id="hello-world">Hello world</${tag}></a>`,
       );
     }),
   );
@@ -28,6 +28,38 @@ it('should ignore anchor tags without headings', async () => {
     ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'].map(async (tag) => {
       const result = await processor.process(`<${tag}>Hello world</${tag}>`);
       expect(result.toString()).toBe(`<${tag}>Hello world</${tag}>`);
+    }),
+  );
+});
+
+it('should add specify the className when option is specified', async () => {
+  const processor = rehype()
+    .data('settings', { fragment: true })
+    .use(rehypeSlug)
+    .use(rehypeHeadingAnchor, { className: ['heading-anchor'] });
+
+  await Promise.all(
+    ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'].map(async (tag) => {
+      const result = await processor.process(`<${tag}>Hello world</${tag}>`);
+      expect(result.toString()).toBe(
+        `<a href="#hello-world" class="heading-anchor"><${tag} id="hello-world">Hello world</${tag}></a>`,
+      );
+    }),
+  );
+});
+
+it('should add multiple classnames when the option is specified', async () => {
+  const processor = rehype()
+    .data('settings', { fragment: true })
+    .use(rehypeSlug)
+    .use(rehypeHeadingAnchor, { className: ['custom', 'name'] });
+
+  await Promise.all(
+    ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'].map(async (tag) => {
+      const result = await processor.process(`<${tag}>Hello world</${tag}>`);
+      expect(result.toString()).toBe(
+        `<a href="#hello-world" class="custom name"><${tag} id="hello-world">Hello world</${tag}></a>`,
+      );
     }),
   );
 });
